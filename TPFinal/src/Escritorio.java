bf39a6842b0d3a790e523a5d923cbbb780c4f288
notsag01 <79101798+notsag01@users.noreply.github.com>
2023-09-19 16:51:35 -0300

    Escritorio Altas

--- TPFinal/src/Escritorio.java
+++ TPFinal/src/Escritorio.java
@@ -1,4 +1,5 @@
 
+import Data.Altas;
 import javax.swing.JFrame;
 
 
@@ -17,6 +18,12 @@ public class Escritorio extends javax.swing.JFrame {
     private Escritorio() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    
+    private void nuevaAlta(){
+        Altas altas = new Altas();
+        altas.setVisible(true);
+        jPanel_escritorio.add(altas);
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -24,7 +31,7 @@ public class Escritorio extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel_usuario = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
+        jPanel_escritorio = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
@@ -41,21 +48,21 @@ public class Escritorio extends javax.swing.JFrame {
         jLabel_usuario.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel_usuario.setOpaque(true);
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
-        jPanel2.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
+        jPanel_escritorio.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel_escritorio.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
 
         jLabel2.setBackground(new java.awt.Color(102, 102, 255));
         jLabel2.setOpaque(true);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPanel_escritorioLayout = new javax.swing.GroupLayout(jPanel_escritorio);
+        jPanel_escritorio.setLayout(jPanel_escritorioLayout);
+        jPanel_escritorioLayout.setHorizontalGroup(
+            jPanel_escritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+        jPanel_escritorioLayout.setVerticalGroup(
+            jPanel_escritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel_escritorioLayout.createSequentialGroup()
                 .addGap(0, 753, Short.MAX_VALUE)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
@@ -65,14 +72,14 @@ public class Escritorio extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jLabel_usuario, javax.swing.GroupLayout.DEFAULT_SIZE, 1421, Short.MAX_VALUE)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel_escritorio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jLabel_usuario, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel_escritorio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
@@ -123,7 +130,7 @@ public class Escritorio extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItem_altasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem_altasActionPerformed
-        // TODO add your handling code here:
+        nuevaAlta();
     }//GEN-LAST:event_jMenuItem_altasActionPerformed
 
     /**
@@ -169,6 +176,6 @@ public class Escritorio extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem_altas;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel_escritorio;
     // End of variables declaration//GEN-END:variables
 }
